51abdd6be96cdd28071b65cb57acac2c17facf52
Nekyia <matthieujullien42@gmail.com>
2016-12-13 16:38:24 +0100

    Update ft_putendl_fd.c

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -16,11 +16,7 @@ void	ft_putendl_fd(char const *s, int fd)
 {
 	if (s)
 	{
-		while (*s != '\0')
-		{
-			ft_putchar_fd(*s, fd);
-			s++;
-		}
+		ft_putstr_fd(s, fd);
 		ft_putchar_fd('\n', fd);
 	}
 }
